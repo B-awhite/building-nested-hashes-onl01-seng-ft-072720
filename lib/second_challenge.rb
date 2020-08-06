f93915f7610881352a213846ae033059c4632fcc
Bria White <bria.awhite@gmail.com>
2020-08-06 20:36:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
+    
     :capulet => {
       :partiarch => {},
       :matriarch => {},
